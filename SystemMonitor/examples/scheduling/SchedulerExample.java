a4e285e4814dfbe13df650f50caf484c7792d6eb
Felix Scheidel <felixscheidel@gmail.com>
2017-06-07 21:34:27 +0000

    added pictures for bulk operations

--- SystemMonitor/examples/scheduling/SchedulerExample.java
+++ SystemMonitor/examples/scheduling/SchedulerExample.java
@@ -1,8 +1,24 @@
 package scheduling;
 
 import io.reactivex.Observable;
+import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
 
 public class SchedulerExample
 {
-	Observable<String> obs0 = Observable.just("Java", "Kotlin", "Scala");
+	public static void main(String[] args) throws InterruptedException
+	{
+		Observable<String> obs0 = Observable.just("Java", "Kotlin", "Scala");
+		Predicate<String> p = new Predicate<String>()
+		{
+
+			@Override
+			public boolean test(String arg0) throws Exception
+			{
+				return arg0.contains("a");
+			}
+		};
+		obs0.subscribeOn(Schedulers.computation()).map(s -> s.toLowerCase()).filter(p).subscribe(s -> {System.out.println(s);});
+		Thread.sleep(1000);
+	}
 }
