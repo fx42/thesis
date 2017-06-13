c784c0a5afe053548f1128ec60ce970ee06a49b7
Scheidel <felix.scheidel@pentasys.de>
2017-06-13 16:54:01 +0200

    changed section schedulers for better understanding. Also changed Code for same reasons

--- SystemMonitor/examples/scheduling/SchedulerExample.java
+++ SystemMonitor/examples/scheduling/SchedulerExample.java
@@ -8,33 +8,34 @@ public class SchedulerExample
 {
 	public static void main( String[] args ) throws InterruptedException
 	{
-		Observable< String > obs0 = Observable.just( "Java", "Kotlin", "Scala" );
+		Observable< String > observable = Observable.just( "Java", "Kotlin", "Scala" );
 		// Aufruf 1
-		obs0.subscribeOn( Schedulers.newThread() ).map( foo ).subscribe( s -> {
+		observable.subscribeOn( Schedulers.newThread() ).map( mapping ).subscribe( s -> {
 			System.out.println( s + ":	" + Thread.currentThread().getName() );
 		} );
 		// Aufruf 2
-		obs0.subscribeOn( Schedulers.newThread() ).map( foo ).subscribeOn( Schedulers.newThread() ).subscribe( s -> {
-			System.out.println( s + ":	" + Thread.currentThread().getName() );
-		} );
+		observable.subscribeOn( Schedulers.newThread() ).map( mapping ).subscribeOn( Schedulers.newThread() )
+				.subscribe( s -> {
+					System.out.println( s + ":	" + Thread.currentThread().getName() );
+				} );
 
 		// Aufruf 3
-		obs0.map( foo ).observeOn( Schedulers.newThread() ).subscribe( s -> {
+		observable.map( mapping ).observeOn( Schedulers.newThread() ).subscribe( s -> {
 			System.out.println( s + ":	" + Thread.currentThread().getName() );
 		} );
 		// Aufruf 4
-		obs0.observeOn( Schedulers.newThread() ).map( foo ).subscribe( s -> {
+		observable.observeOn( Schedulers.newThread() ).map( mapping ).subscribe( s -> {
 			System.out.println( s + ":	" + Thread.currentThread().getName() );
 		} );
 		// Aufruf 5
-		obs0.observeOn( Schedulers.newThread() ).map( foo ).observeOn( Schedulers.newThread() ).subscribe( s -> {
+		observable.observeOn( Schedulers.newThread() ).map( mapping ).observeOn( Schedulers.newThread() ).subscribe( s -> {
 			System.out.println( s + ":	" + Thread.currentThread().getName() );
 		} );
 
 		Thread.sleep( 5000 );
 	}
 
-	static Function< String, String > foo = new Function< String, String >()
+	static Function< String, String > mapping = new Function< String, String >()
 	{
 
 		@Override
--- Tex/Hauptdokument.lol
+++ Tex/Hauptdokument.lol
@@ -6,12 +6,13 @@
 \contentsline {lstlisting}{\numberline {6.2}Beispiel Cold Observable}{18}{lstlisting.6.2}
 \contentsline {lstlisting}{\numberline {6.3}Beispiel Hot Observable}{18}{lstlisting.6.3}
 \contentsline {lstlisting}{\numberline {6.4}Beispiel Flowable mit BackPressureBuffer}{20}{lstlisting.6.4}
-\contentsline {lstlisting}{\numberline {6.5}DefaultSubscriber mit Back Pressure}{21}{lstlisting.6.5}
+\contentsline {lstlisting}{\numberline {6.5}DefaultSubscriber mit Back Pressure}{20}{lstlisting.6.5}
 \contentsline {lstlisting}{\numberline {6.6}Beispiel eines Singles}{21}{lstlisting.6.6}
 \contentsline {lstlisting}{\numberline {6.7}Beispielverwendung eines Subjects}{22}{lstlisting.6.7}
 \contentsline {lstlisting}{\numberline {6.8}Beispielverwendung eines Processors}{23}{lstlisting.6.8}
-\contentsline {lstlisting}{\numberline {6.9}Beispiele f\IeC {\"u}r Scheduling}{24}{lstlisting.6.9}
-\contentsline {lstlisting}{\numberline {6.10}Beispiel Filter Operation}{26}{lstlisting.6.10}
-\contentsline {lstlisting}{\numberline {6.11}Beispiel Map Operation}{27}{lstlisting.6.11}
-\contentsline {lstlisting}{\numberline {6.12}Beispiel Merge Operation}{28}{lstlisting.6.12}
-\contentsline {lstlisting}{\numberline {6.13}Beispiel Zip Operation}{29}{lstlisting.6.13}
+\contentsline {lstlisting}{\numberline {6.9}Beispiele f\IeC {\"u}r Scheduling mit zwei subscribeOn-Aufrufen}{24}{lstlisting.6.9}
+\contentsline {lstlisting}{\numberline {6.10}Beispiele f\IeC {\"u}r Scheduling mit zwei observeOn-Aufrufen}{25}{lstlisting.6.10}
+\contentsline {lstlisting}{\numberline {6.11}Beispiel Filter Operation}{26}{lstlisting.6.11}
+\contentsline {lstlisting}{\numberline {6.12}Beispiel Map Operation}{26}{lstlisting.6.12}
+\contentsline {lstlisting}{\numberline {6.13}Beispiel Merge Operation}{27}{lstlisting.6.13}
+\contentsline {lstlisting}{\numberline {6.14}Beispiel Zip Operation}{28}{lstlisting.6.14}
